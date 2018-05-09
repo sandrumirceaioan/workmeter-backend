1451b0b319b02399b0ec4a396f09e174d1b96c43
sandru mircea <sandrumirceaioan@gmail.com>
2018-05-09 17:06:18 +0300

    dep to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,32 @@
+import * as env from 'dotenv';
+import 'reflect-metadata';
+import * as logger from 'morgan';
+env.config();
+
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as path from 'path';
+
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { initDocumentation } from './documentation';
+import { EntitiesExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
-	/* body parser */
-	const instance = express();
-	instance.use(bodyParser.json());
-	instance.use(bodyParser.urlencoded({ extended: false }));
+    const server = express();
+    server.use(logger(process.env.NODE_ENV));
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: false }));
 
-	const app = await NestFactory.create(ApplicationModule);
-	/* app.useGlobalFilters(new HttpExceptionFilter()); */
-	/* app.useGlobalGuards(new AuthGuard()); */
+    const app = await NestFactory.create(ApplicationModule, server);
+    app.useGlobalFilters(new EntitiesExceptionFilter());
+    initDocumentation(app, {
+        version: '0.0.1',
+        description: 'Nest boilerplate description.',
+        title: 'Nest boilerplate',
+        endpoint: '/docs'
+    });
 
-	/* set view engine to html */
-	app.use(express.static(path.join(__dirname, 'dist')));
-	app.set('views', __dirname + '/dist');
-	app.set('view engine', 'html');
-	app.setGlobalPrefix('/api');
-
-  await app.listen(3000);
+    await app.listen(parseInt(process.env.PORT) || 3000);
 }
+
 bootstrap();
