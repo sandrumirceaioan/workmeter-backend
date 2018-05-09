409ad253e411d3b2fc74ec7519ed8f4d79fd50f1
sandru mircea <sandrumirceaioan@gmail.com>
2018-05-09 17:41:22 +0300

    appnest

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,32 @@
+import * as env from 'dotenv';
+import 'reflect-metadata';
+import * as logger from 'morgan';
+env.config();
+
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as path from 'path';
+
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { initDocumentation } from './documentation';
+import { EntitiesExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
-	/* body parser */
-	const instance = express();
-	instance.use(bodyParser.json());
-	instance.use(bodyParser.urlencoded({ extended: false }));
+    const server = express();
+    server.use(logger(process.env.NODE_ENV));
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: false }));
 
-	const app = await NestFactory.create(ApplicationModule);
-	/* app.useGlobalFilters(new HttpExceptionFilter()); */
-	/* app.useGlobalGuards(new AuthGuard()); */
-
-	app.setGlobalPrefix('/api');
-  await app.listen(3000);
+    const app = await NestFactory.create(ApplicationModule, server);
+    app.useGlobalFilters(new EntitiesExceptionFilter());
+    initDocumentation(app, {
+        version: '0.0.1',
+        description: 'Nest boilerplate description.',
+        title: 'Nest boilerplate',
+        endpoint: '/docs'
+    });
+		app.setGlobalPrefix('/api');
+    await app.listen(parseInt(process.env.PORT) || 3000);
 }
+
 bootstrap();
