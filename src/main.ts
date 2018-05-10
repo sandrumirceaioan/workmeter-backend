fee447db623f9f90bd7bd643a2f013bb3758841a
Sandru Mircea <sandrumirceaioan@gmail.com>
2018-05-10 11:03:02 +0300

    working app on heroku

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,12 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
-	/* body parser */
-	const instance = express();
-	instance.use(bodyParser.json());
-	instance.use(bodyParser.urlencoded({ extended: false }));
 
 	const app = await NestFactory.create(ApplicationModule);
 	/* app.useGlobalFilters(new HttpExceptionFilter()); */
 	/* app.useGlobalGuards(new AuthGuard()); */
 
 	/* set view engine to html */
-	app.use(express.static(path.join(__dirname, 'dist')));
-	app.set('views', __dirname + '/dist');
-	app.set('view engine', 'html');
 	app.setGlobalPrefix('/api');
 
   	await app.listen(process.env.PORT || 3000)
